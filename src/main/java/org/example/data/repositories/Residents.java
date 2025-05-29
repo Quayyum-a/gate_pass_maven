ed6f9e00cd57f4edc14ed5a703ab364724d3a392
Quayyum-a <quayyumariyo@gmail.com>
2025-05-29 12:39:11 +0100

    add Residents repository interface for MongoDB

---
+++ src/main/java/org/example/data/repositories/Residents.java
@@ -0,0 +1,8 @@
+package org.example.data.repositories;
+
+import org.example.data.models.Resident;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface Residents extends MongoRepository<Resident, String> {
+
+}
