b66c7783e4df17d09e42bc9fec74926312290666
Quayyum-a <quayyumariyo@gmail.com>
2025-05-29 12:37:52 +0100

    Add AccessToken model for managing access tokens in the application

---
+++ src/main/java/org/example/data/models/AccessToken.java
@@ -0,0 +1,26 @@
+package org.example.data.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "access_tokens")
+public class AccessToken {
+    @Id
+    private String id;
+    private String token;
+    private LocalDateTime creationDate;
+    private LocalDateTime expiryDate;
+    private boolean isUsed;
+    @DBRef
+    private Resident resident;
+    @DBRef
+    private Visitor visitor;
+    private String visitorPhoneNumber;
+
+
+}
