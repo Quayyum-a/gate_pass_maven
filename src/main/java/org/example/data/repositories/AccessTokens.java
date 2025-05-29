f4ec9017881f822e4367e2015dc94f864ebc4845
Quayyum-a <quayyumariyo@gmail.com>
2025-05-29 12:38:16 +0100

    Add AccessTokens repository interface for MongoDB

---
+++ src/main/java/org/example/data/repositories/AccessTokens.java
@@ -0,0 +1,7 @@
+package org.example.data.repositories;
+
+import org.example.data.models.AccessToken;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AccessTokens extends MongoRepository<AccessToken, String> {
+}
