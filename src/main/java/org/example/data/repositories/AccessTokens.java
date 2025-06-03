57f63a8e3ead8d927f43721b89fe52e0bd3e2cd4
Quayyum-a <quayyumariyo@gmail.com>
2025-06-03 13:45:50 +0100

    Add tests for Resident entity: save, find by ID, and delete by ID

--- src/main/java/org/example/data/repositories/AccessTokens.java
+++ src/main/java/org/example/data/repositories/AccessTokens.java
@@ -4,4 +4,5 @@ import org.example.data.models.AccessToken;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AccessTokens extends MongoRepository<AccessToken, String> {
+
 }
