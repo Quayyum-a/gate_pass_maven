22c610e9affaadf9740da78a984a6c7a88d54b66
Quayyum-a <quayyumariyo@gmail.com>
2025-06-03 15:56:33 +0100

    Add

--- src/main/java/org/example/dtos/response/RegisterResidentResponse.java
+++ src/main/java/org/example/dtos/response/RegisterResidentResponse.java
@@ -1,4 +1,12 @@
 package org.example.dtos.response;
 
+import lombok.Data;
+
+@Data
 public class RegisterResidentResponse {
+    private String fullName;
+    private String email;
+    private String Address;
+
+
 }
