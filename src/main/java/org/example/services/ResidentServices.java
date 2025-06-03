030b6de6e15316a575cf868f058c895aaa1cabf8
Quayyum-a <quayyumariyo@gmail.com>
2025-06-03 15:56:01 +0100

    Add

--- src/main/java/org/example/services/ResidentServices.java
+++ src/main/java/org/example/services/ResidentServices.java
@@ -2,6 +2,8 @@ package org.example.services;
 
 import org.example.dtos.request.GenerateAccessTokenRequest;
 import org.example.dtos.request.LoginResidentRequest;
+import org.example.dtos.request.RegisterResidentRequest;
+import org.example.dtos.response.GenerateAccessTokenResponse;
 import org.example.dtos.response.LoginResidentResponse;
 import org.example.dtos.response.RegisterResidentResponse;
 
