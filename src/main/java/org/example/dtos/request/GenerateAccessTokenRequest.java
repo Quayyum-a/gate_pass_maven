22595c442dc50ea36ead4fcd8da7cb74185e0f34
Quayyum-a <quayyumariyo@gmail.com>
2025-06-03 15:57:28 +0100

    Add

--- src/main/java/org/example/dtos/request/GenerateAccessTokenRequest.java
+++ src/main/java/org/example/dtos/request/GenerateAccessTokenRequest.java
@@ -1,4 +1,11 @@
 package org.example.dtos.request;
 
+import lombok.Data;
+
+@Data
 public class GenerateAccessTokenRequest {
+    private String email;
+    private String visitorName;
+    private String visitorPhoneNumber;
+    private String whomToSee;
 }
