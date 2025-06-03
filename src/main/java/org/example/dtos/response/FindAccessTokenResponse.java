051291deb51389708a533253b384f7b5a313387d
Quayyum-a <quayyumariyo@gmail.com>
2025-06-03 20:46:48 +0100

    add AccessTokenNotFound exception class

--- src/main/java/org/example/dtos/response/FindAccessTokenResponse.java
+++ src/main/java/org/example/dtos/response/FindAccessTokenResponse.java
@@ -1,14 +1,28 @@
 package org.example.dtos.response;
 
 import lombok.Data;
+import org.example.data.models.AccessToken;
 
 @Data
 public class FindAccessTokenResponse {
-    private String accessToken;
     private String visitorName;
     private String visitorPhoneNumber;
     private String whomToSee;
     private String residentFullName;
+    private String residentAddress;
     private String residentEmail;
     private boolean isValid;
+
+    public AccessToken getAccessToken() {
+        AccessToken accessToken = new AccessToken();
+        accessToken.setVisitorName(visitorName);
+        accessToken.setVisitorPhoneNumber(visitorPhoneNumber);
+        accessToken.setWhomToSee(whomToSee);
+        return accessToken;
+    }
+
+    public void setIsValid(boolean b) {
+        isValid = b;
+    }
 }
+
