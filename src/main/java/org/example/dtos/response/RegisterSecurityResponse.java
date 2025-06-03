0ea129f1a11c10798647351cad9ec969db3e3c6c
Quayyum-a <quayyumariyo@gmail.com>
2025-06-03 21:15:14 +0100

    Add

--- src/main/java/org/example/dtos/response/RegisterSecurityResponse.java
+++ src/main/java/org/example/dtos/response/RegisterSecurityResponse.java
@@ -5,5 +5,11 @@ import lombok.Data;
 @Data
 public class RegisterSecurityResponse {
     private String message;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
 
+    public RegisterSecurityResponse() {
+        this.message = "Security registration successful";
+    }
 }
