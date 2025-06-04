25c63de4c28d2032d5a5cc96d6eacd263627017b
Quayyum-a <quayyumariyo@gmail.com>
2025-06-04 16:06:35 +0100

    Add

--- src/main/java/org/example/services/ResidentServices.java
+++ src/main/java/org/example/services/ResidentServices.java
@@ -1,8 +1,10 @@
 package org.example.services;
 
+import org.example.dtos.request.FindAccessToken;
 import org.example.dtos.request.GenerateAccessTokenRequest;
 import org.example.dtos.request.LoginResidentRequest;
 import org.example.dtos.request.RegisterResidentRequest;
+import org.example.dtos.response.FindAccessTokenResponse;
 import org.example.dtos.response.GenerateAccessTokenResponse;
 import org.example.dtos.response.LoginResidentResponse;
 import org.example.dtos.response.RegisterResidentResponse;
@@ -11,4 +13,6 @@ public interface ResidentServices {
     RegisterResidentResponse register(RegisterResidentRequest request);
     LoginResidentResponse login(LoginResidentRequest request);
     GenerateAccessTokenResponse generateAccessToken(GenerateAccessTokenRequest request);
+
+    FindAccessTokenResponse findAccessToken(FindAccessToken residentRequest);
 }
