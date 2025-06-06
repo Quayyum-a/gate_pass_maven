86e4db793ab47f15e7906556722f5f959a6f6573
Quayyum-a <quayyumariyo@gmail.com>
2025-06-06 06:59:19 +0100

    Add expiryDate field to FindAccessTokenResponse

--- src/main/java/org/example/dtos/response/ApiResponse.java
+++ src/main/java/org/example/dtos/response/ApiResponse.java
@@ -3,9 +3,26 @@ package org.example.dtos.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Generic API response wrapper
+ * @param <T> The type of the data being returned
+ */
 @Data
 @AllArgsConstructor
-public class ApiResponse {
-    private Object data;
+public class ApiResponse<T> {
+    private T data;
     private boolean success;
+    private String message;
+
+    public ApiResponse(T data, String message) {
+        this.data = data;
+        this.success = true;
+        this.message = message;
+    }
+
+    public ApiResponse(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+        this.data = null;
+    }
 }
