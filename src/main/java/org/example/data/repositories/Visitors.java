b2488eb235f9f17dcaca8da2492bf3a1e82a8e40
Quayyum-a <quayyumariyo@gmail.com>
2025-06-06 11:38:35 +0100

    Add query methods to Visitors repository for date range and search term

--- src/main/java/org/example/data/repositories/Visitors.java
+++ src/main/java/org/example/data/repositories/Visitors.java
@@ -2,6 +2,14 @@ package org.example.data.repositories;
 
 import org.example.data.models.Visitor;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface Visitors extends MongoRepository<Visitor, String> {
+    @Query("{'whomToSee.id': ?0, 'checkIn': {$gte: ?1, $lte: ?2}}")
+    List<Visitor> findByResidentIdAndDateRange(String residentId, LocalDateTime fromDate, LocalDateTime toDate);
+    @Query("{'$or': [{'fullName': {$regex: ?0, $options: 'i'}}, {'phoneNumber': {$regex: ?0, $options: 'i'}}]}")
+    List<Visitor> findBySearchTerm(String search);
 }
