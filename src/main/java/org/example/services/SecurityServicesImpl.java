76018076f297c58a9a06a30dfce53dbf1c1eacd5
Quayyum-a <quayyumariyo@gmail.com>
2025-06-06 05:40:52 +0100

    Refactor SecurityServicesImpl to improve dependency injection and streamline registration and login processes

--- src/main/java/org/example/services/SecurityServicesImpl.java
+++ src/main/java/org/example/services/SecurityServicesImpl.java
@@ -1,73 +1,61 @@
 package org.example.services;
 
-import org.example.data.models.AccessToken;
+import org.example.data.models.Security;
 import org.example.data.repositories.AccessTokens;
-import org.example.data.repositories.Residents;
 import org.example.data.repositories.Securities;
-import org.example.dtos.request.FindAccessToken;
+import org.example.dtos.request.FindAccessTokenRequest;
 import org.example.dtos.request.LoginSecurityRequest;
 import org.example.dtos.request.RegisterSecurityRequest;
 import org.example.dtos.response.FindAccessTokenResponse;
 import org.example.dtos.response.LoginSecurityResponse;
 import org.example.dtos.response.RegisterSecurityResponse;
+import org.example.utils.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static org.example.verification.Verification.*;
+import static org.example.services.ResidentServicesImpl.getFindAccessTokenResponse;
+import static org.example.verification.VerificationNew.*;
 
 @Service
 public class SecurityServicesImpl implements SecurityServices {
-    @Autowired
-    private Securities securityRepository;
+    private final Securities securityRepository;
+    private final AccessTokens accessTokenRepository;
 
     @Autowired
-    private Residents residentRepository;
-    
-    @Autowired
-    private AccessTokens accessTokenRepository;
-    @Autowired
-    private Securities securities;
+    public SecurityServicesImpl(Securities securityRepository, AccessTokens accessTokenRepository) {
+        this.securityRepository = securityRepository;
+        this.accessTokenRepository = accessTokenRepository;
+    }
 
     @Override
     public RegisterSecurityResponse register(RegisterSecurityRequest request) {
-        RegisterSecurityResponse response = new RegisterSecurityResponse();
+        verifyEmailAndPassword(request.getEmail(), request.getPassword());
         verifyEmail(request.getEmail());
-        RegisterSecurityRequest requestRegister = new RegisterSecurityRequest();
-        requestRegister.setFullName(request.getFullName());
-        requestRegister.setEmail(request.getEmail());
-        requestRegister.setPassword(request.getPassword());
-        requestRegister.setPhoneNumber(request.getPhoneNumber());
-        securities.save(requestRegister);
+
+        Security security = Mapper.mapToRegisterSecurityRequest(request);
+        Security savedSecurity = securityRepository.save(security);
+
+        RegisterSecurityResponse response = new RegisterSecurityResponse();
+        response.setFullName(savedSecurity.getFullName());
+        response.setEmail(savedSecurity.getEmail());
+        response.setPhoneNumber(savedSecurity.getPhoneNumber());
         response.setMessage("Security registered successfully");
         return response;
     }
 
-
-
     @Override
     public LoginSecurityResponse login(LoginSecurityRequest request) {
         verifyEmailAndPassword(request.getEmail(), request.getPassword());
+        verifyEmail(request.getEmail());
+
         LoginSecurityResponse response = new LoginSecurityResponse();
-        response.setMessage("Security logged in successfully");
+        response.setMessage("Login successful");
         return response;
     }
-
-
 
     @Override
-    public Object findAccessToken(FindAccessToken request) {
-        FindAccessTokenResponse response = new FindAccessTokenResponse();
-        AccessToken accessToken = accessTokenRepository.findByToken(request.getAccessCode());
-        verifyToken(accessToken);
-        response.setVisitorName(accessToken.getVisitorName());
-        response.setVisitorPhoneNumber(accessToken.getVisitorPhoneNumber());
-        response.setWhomToSee(accessToken.getWhomToSee());
-        response.setResidentFullName(accessToken.getResident().getFullName());
-        response.setResidentAddress(accessToken.getResident().getAddress());
-        response.setResidentEmail(accessToken.getResident().getEmail());
-        response.setIsValid(true);
-        return response;
+    public FindAccessTokenResponse findAccessToken(FindAccessTokenRequest request) {
+        verifyToken(request.getToken());
+        return getFindAccessTokenResponse(request, accessTokenRepository);
     }
-
-
 }
