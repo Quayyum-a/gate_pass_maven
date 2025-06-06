a39244c43643c6f89fc9ca373809acec48922054
Quayyum-a <quayyumariyo@gmail.com>
2025-06-06 11:28:53 +0100

    add status field to AccessToken for state management

--- src/main/java/org/example/data/repositories/AccessTokens.java
+++ src/main/java/org/example/data/repositories/AccessTokens.java
@@ -2,8 +2,14 @@ package org.example.data.repositories;
 
 import org.example.data.models.AccessToken;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface AccessTokens extends MongoRepository<AccessToken, String> {
-
     AccessToken findByToken(String token);
+    List<AccessToken> findByResidentIdAndStatus(String residentId, String status);
+    List<AccessToken> findByResidentId(String residentId);
+    @Query("{'$or': [{'visitorName': {$regex: ?0, $options: 'i'}}, {'token': {$regex: ?0, $options: 'i'}}], 'resident.id': ?1}")
+    List<AccessToken> findBySearchTermAndResidentId(String search, String residentId);
 }
